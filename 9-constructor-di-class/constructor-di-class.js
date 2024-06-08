0fce11c86c35f0b0c0acfe0a0affe7b088b2d96e
Arief-Karditya <nolifegansaya@gmail.com>
2024-06-08 15:35:03 +0700

    Constructor di Class, JS OOP

---
+++ 9-constructor-di-class/constructor-di-class.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name) {
+        console.info(`Membuat Person dengan nama ${name}`);
+    }
+}
+
+const arief = new Person("Arief Karditya Hermawan");
+console.info(arief);
+
+const hilmi = new Person("Hilmi Akbar Muharrom")
